eb2ecdc11c6e24ca9fafb38fa5fa7214b1f4e128
PabloGlezAlv <62878159+PabloGlezAlv@users.noreply.github.com>
2024-02-08 18:31:30 +0100

    start of objects

---
+++ Game/Assets/Scripts/ObjectMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float amplitude = 1f; 
+
+    [SerializeField]
+    private float frequency = 1f;  
+
+    private bool onFloor = true;  // Levitate or not
+
+    private float originalY;  
+    private Rigidbody2D rigidbody2D;
+
+    void Start()
+    {
+        originalY = transform.position.y;
+
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (onFloor)
+        {
+            float newY = originalY + amplitude * Mathf.Sin(frequency * Time.time);
+
+            rigidbody2D.MovePosition(new Vector2(transform.position.x, newY));
+        }
+    }
+}
---
+++ Game/Assets/Scripts/ShadowMovement.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class ShadowMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float frequency = 1f;
+
+    [SerializeField]
+    private float minorScale = -0.8f;
+    [SerializeField]
+    private float maxScale = 0.8f;
+
+    private bool onFloor = true;  // Levitate or not
+
+    private float scaleX = 0;
+    private float scaleY = 0;
+
+    void Start()
+    {
+        scaleX = transform.localScale.x; 
+        scaleY = transform.localScale.y;
+    }
+
+    void Update()
+    {
+        if (onFloor)
+        {
+            float mappedValue = Mathf.Clamp01(((Mathf.Sin(Time.time * frequency) + 1f) / 2f) * (maxScale - minorScale) + minorScale);
+
+            float realScaleX = scaleX - scaleX * mappedValue;
+            float realScaleY = scaleY - scaleY * mappedValue;
+
+            // Aplica la nueva escala al objeto
+            transform.localScale = new Vector3(realScaleX, realScaleY, 1f);
+        }
+    }
+}
