ad3b959847a65f59b0be6eb3b0b1159ad064246b
PabloGlezAlv <62878159+PabloGlezAlv@users.noreply.github.com>
2024-02-17 17:44:39 +0100

    bullet start

---
+++ Game/Assets/BulletMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    private int damage = 0;
+
+    private Vector3 direction;
+
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
