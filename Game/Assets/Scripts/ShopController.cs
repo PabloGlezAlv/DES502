b2fde32f9fa9729375fdb30236fe6a59fe1a2388
PabloGlezAlv <62878159+PabloGlezAlv@users.noreply.github.com>
2024-02-20 10:58:46 +0100

    fixed shop bug

--- Game/Assets/Scripts/ShopController.cs
+++ Game/Assets/Scripts/ShopController.cs
@@ -1,38 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
+using TMPro;
+using CodeMonkey.Utils;
 
 public class ShopController : MonoBehaviour
 {
-    [SerializeField]
-    GameObject container;
-    [SerializeField]
-    GameObject template;
-    void Awake()
+
+    private Transform container;
+    private Transform shopItemTemplate;
+
+    private void Awake()
     {
-        template.SetActive(false);
+        container = transform.Find("container");
+        shopItemTemplate = container.Find("shopItemTemplate");
+        shopItemTemplate.gameObject.SetActive(false);
     }
 
     private void Start()
     {
-        CreateItemButtom(ObjectsManager.instance.getCoinSprite(0), "MONEDA", ObjectsManager.instance.getSpeedPrice(0), 0);
-        CreateItemButtom(ObjectsManager.instance.getScaleSprite(0), "Iman", ObjectsManager.instance.getSpeedPrice(2), 1);
-        CreateItemButtom(ObjectsManager.instance.getSpeedSprite(0), "Spped", ObjectsManager.instance.getSpeedPrice(3), 2);
+        CreateItemButton(Items.SpeedHelmet, ObjectsManager.instance.getCoinSprite(0), "MONEDA", ObjectsManager.instance.getSpeedPrice(0), 0);
+        CreateItemButton(Items.SpeedHelmet, ObjectsManager.instance.getScaleSprite(0), "Iman", ObjectsManager.instance.getSpeedPrice(2), 1);
+        CreateItemButton(Items.SpeedHelmet, ObjectsManager.instance.getSpeedSprite(0), "Spped", ObjectsManager.instance.getSpeedPrice(3), 2);
     }
 
-    private void CreateItemButtom(Sprite sprite, string name, int cost, int positionIndex)
+    private void CreateItemButton(Items itemType, Sprite itemSprite, string itemName, int itemCost, int positionIndex)
     {
-        Transform shopItemTransform = Instantiate(template.transform, container.transform);
+        Transform shopItemTransform = Instantiate(shopItemTemplate, container);
+        shopItemTransform.gameObject.SetActive(true);
         RectTransform shopItemRectTransform = shopItemTransform.GetComponent<RectTransform>();
-        
+
         float shopItemHeight = 450f;
         shopItemRectTransform.anchoredPosition = new Vector2(shopItemHeight * positionIndex - shopItemHeight, 0);
-        shopItemTransform.Find("itemName").GetComponent<TextMeshProUGUI>().SetText(name);
-        shopItemTransform.Find("costText").GetComponent<TextMeshProUGUI>().SetText(cost.ToString());
 
-        shopItemTransform.Find("itemImage");
+        shopItemTransform.Find("nameText").GetComponent<TextMeshProUGUI>().SetText(itemName);
+        shopItemTransform.Find("costText").GetComponent<TextMeshProUGUI>().SetText(itemCost.ToString());
+
+        shopItemTransform.Find("itemImage").GetComponent<Image>().sprite = itemSprite;
+
+        shopItemTransform.GetComponent<Button_UI>().ClickFunc = () => {
+            // Clicked on shop item button
+            TryBuyItem(itemType);
+        };
     }
+
+    private void TryBuyItem(Items itemType)
+    {
+
+    }
+
 }
