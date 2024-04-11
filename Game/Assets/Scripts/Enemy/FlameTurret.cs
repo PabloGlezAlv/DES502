2b1b4a4c38d93bee1bc07130837fdc84ec232453
CrispulentCrisps <70386744+CrispulentCrisps@users.noreply.github.com>
2024-04-11 16:09:27 +0100

    Turret and flame sprites, may need some work

--- Game/Assets/Scripts/Enemy/FlameTurret.cs
+++ Game/Assets/Scripts/Enemy/FlameTurret.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using UnityEngine;
 
 public class FlameTurret : MonoBehaviour
@@ -14,6 +15,12 @@ public class FlameTurret : MonoBehaviour
         NoLoop,
         Forward,
         PingPong,
+    }   
+    protected enum EnemyDir
+    {
+        Up = 0, 
+        Down = 1, 
+        Side = 2,
     }
 
     private Vector2 CenterPos;
@@ -22,11 +29,14 @@ public class FlameTurret : MonoBehaviour
     private bool PingPongFlag = false;
 
     [SerializeField]
+    private EnemyDir Directions;
+    private int ChosenDir;
+    [SerializeField]
     private float Speed;
     [SerializeField]
     private float FlameSpeed;
     [SerializeField]
-    private GameObject Flame;
+    private GameObject[] Flame;
     private float FlameTime;
     [SerializeField]
     private MovementType MType;
@@ -36,26 +46,51 @@ public class FlameTurret : MonoBehaviour
     [SerializeField]
     private Vector2[] Points;
 
+    Animator anim;
+
     private bool Active = true;
 
     private int LineIndex;
 
     private void Start()
     {
+        anim = GetComponent<Animator>();
+        switch (Directions)
+        {
+            case EnemyDir.Up:
+                anim.SetInteger("DecideDirection", 0);
+                ChosenDir = 0;
+                break;
+            case EnemyDir.Down:
+                anim.SetInteger("DecideDirection", 1);
+                ChosenDir = 1;
+                break;
+            case EnemyDir.Side:
+                anim.SetInteger("DecideDirection", 2);
+                ChosenDir = 2;
+                break;
+            default:
+                break;
+        }
         for (int i = 0; i < Points.Length; i++)
         {
             Points[i] += (Vector2)transform.parent.position;
         }
 
-        transform.position = Points[0];
+        if(Points.Length > 0) transform.position = Points[0];
         CenterPos = transform.position;
-        Flame.active = false;
+        for (int i = 0; i < Flame.Length; i++)
+        {
+            Flame[i].active = false;
+        }
         if (Points.Length == 0) MType = MovementType.NA;
         controller = GameObject.FindGameObjectWithTag("DoorController").GetComponent<DoorsController>();
+
     }
 
     private void FixedUpdate()
     {
+        anim.SetBool("Fire", Flame[ChosenDir].active);
         if (controller != null)
         {
             Active = controller.roomEntities > 0;
@@ -71,9 +106,9 @@ public class FlameTurret : MonoBehaviour
         }
         if (FlameTime > 1f)
         {
-            Flame.active = !Flame.active;
+            Flame[ChosenDir].active = !Flame[ChosenDir].active;
             FlameTime = 0;
-            if (Flame.active)
+            if (Flame[ChosenDir].active)
             {
                 AudioManager.instance.Play("FlameWhoosh");
             }
@@ -92,7 +127,7 @@ public class FlameTurret : MonoBehaviour
     [ExecuteAlways]
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.green;
+        Gizmos.color = UnityEngine.Color.green;
         for (int i = 0; i < Points.Length-1; i++)
         {
             Gizmos.DrawLine(Points[i], Points[i+1]);
